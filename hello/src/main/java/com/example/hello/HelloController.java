a32c49d90ea88359d4a5be75f3f3a5c909153242
custa <custa@126.com>
2018-04-11 09:51:48 +0800

    hello 加入 0~2 秒随机延迟响应

--- hello/src/main/java/com/example/hello/HelloController.java
+++ hello/src/main/java/com/example/hello/HelloController.java
@@ -1,9 +1,13 @@
 package com.example.hello;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Random;
+
+@Slf4j
 @RestController
 public class HelloController {
 
@@ -11,7 +15,10 @@ public class HelloController {
 	private String greeting;
 
 	@RequestMapping("/hello")
-	public String hello() {
+	public String hello() throws InterruptedException {
+		int sleepTime = new Random().nextInt(2000);
+		log.info("Sleep Time: " + sleepTime);
+		Thread.sleep(sleepTime);
 		return "Hello, " + greeting;
 	}
 }
