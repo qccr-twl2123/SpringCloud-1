059720d1f2ca74069c1a624d2deb19448e0d0593
custa <custa@126.com>
2018-04-24 17:46:01 +0800

    stream 基本配置

---
+++ stream/src/main/java/com/example/stream/SinkReceiver.java
@@ -0,0 +1,15 @@
+package com.example.stream;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+
+@Slf4j
+@EnableBinding(Sink.class)
+public class SinkReceiver {
+	@StreamListener(Sink.INPUT)
+	public void receive(Object payload) {
+		log.info("Received: " + payload);
+	}
+}
