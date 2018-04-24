05e9207d74e8bf00881aa8be562efc0e67cc88da
custa <custa@126.com>
2018-04-24 17:47:03 +0800

    ribbon 和 hello 增加 sleuth 依赖，ribbon 和 hello 打印请求头部信息
    请求会增加以下头部：
    x-b3-traceid
    x-b3-spanid
    x-b3-parentspanid
    x-b3-sampled

--- ribbon/src/main/java/com/example/ribbon/ConsumerController.java
+++ ribbon/src/main/java/com/example/ribbon/ConsumerController.java
@@ -1,18 +1,23 @@
 package com.example.ribbon;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 @RestController
 public class ConsumerController {
 	@Autowired
 	HelloService helloService;
 
 	@RequestMapping(value = "/ribbon", method = RequestMethod.GET)
-	public String helloConsumer() {
+	public String helloConsumer(@RequestHeader HttpHeaders headers) {
+		log.info("headers: " + headers.toString());
 		return helloService.helloService();
 	}
 }
