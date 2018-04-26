eb4bf8b34d4dcbefad678d2d56ef015d1f099ec3
custa <custa@126.com>
2018-04-27 01:04:14 +0800

    zipkin 2.7.3
    1. 借鉴 self-contained executable jar（https://zipkin.io/pages/quickstart） 构建
    2. 只能使用 Spring Boot 1.X 版本

---
+++ zipkin/src/main/java/com/example/zipkin/ZipkinApplication.java
@@ -0,0 +1,14 @@
+package com.example.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin.server.internal.EnableZipkinServer;
+
+@EnableZipkinServer
+@SpringBootApplication
+public class ZipkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinApplication.class, args);
+	}
+}
